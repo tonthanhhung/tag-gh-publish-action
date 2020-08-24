40a656c43061608e09718dcae4643d820f0c44c6
Hung Ton <tonthanhhung90@gmail.com>
2020-08-24 15:47:45 +0700

    First commit

---
+++ README.md
@@ -0,0 +1,44 @@
+# tag-gh-publish-action
+
+GitHub action to automatically publish packages to github tag.
+
+## Usage
+
+Create a new `.github/workflows/tag-release.yml` file:
+
+```yaml
+name: tag-release
+on:
+  push:
+    branches:
+      - master # Change this to your default branch
+jobs:
+  tag-release:
+    name: tag-release
+    runs-on: ubuntu-latest
+    steps:
+    - name: Checkout repository
+      uses: actions/checkout@master
+    - name: Set up Node.js
+      uses: actions/setup-node@master
+      with:
+        node-version: 10.0.0
+    - name: Publish if version has been updated
+      uses: tonthanhhung/tag-gh-publish-action@168e35e
+      env: # More info about the environment variables in the README
+        GITHUB_TOKEN: ${{ secrets.GITHUB_TOKEN }} # Leave this as is, it's automatically generated        
+```
+
+Now, when someone changes the version in `package.json` to 1.2.3 and pushes a commit with the message `Release 1.2.3`, the `tag-release` action will create a new tag `v1.2.3` and publish the package to the npm registry.
+
+### Environment variables
+
+- `GITHUB_TOKEN`: this is a token that GitHub generates automatically, you only need to pass it to the action as in the example
+
+## Related projects
+
+- [version-check](https://github.com/EndBug/version-check) allows to define custom workflows based on version changes
+
+## License
+
+[MIT](LICENSE)
---
+++ index.js
@@ -0,0 +1,192 @@
+#!/usr/bin/env node
+const core = require('@actions/core');
+
+const process = require("process");
+const {join} = require("path");
+const {spawn} = require("child_process");
+const {readFile} = require("fs");
+
+async function main() {
+  const dir =
+    process.env.WORKSPACE ||
+    process.env.GITHUB_WORKSPACE ||
+    "/github/workspace";
+
+  const eventFile =
+    process.env.GITHUB_EVENT_PATH || "/github/workflow/event.json";
+  const eventObj = await readJson(eventFile);
+
+  const commitPattern =
+    getEnv("COMMIT_PATTERN") || "^(?:Release|Version) (\\S+)";
+
+  const {name, email} = eventObj.repository.owner;
+
+  const config = {
+    commitPattern,
+    tagAuthor: {name, email}
+  };
+
+  await processDirectory(dir, config, eventObj.commits);
+}
+
+function getEnv(name) {
+  return process.env[name] || process.env[`INPUT_${name}`];
+}
+
+async function getVersion(dir) {
+  const packageFile = join(dir, "package.json");
+  const packageObj = await readJson(packageFile).catch(() =>
+    Promise.reject(
+      new NeutralExitError(`package file not found: ${packageFile}`)
+    )
+  );
+
+  if (packageObj == null || packageObj.version == null) {
+    throw new Error("missing version field!");
+  }
+
+  const {version} = packageObj;
+  return version
+}
+
+async function processDirectory(dir, config, commits) {
+
+  let version = await getVersion(dir)
+  await gitSetup(dir, config)
+
+  await addBuiltPackage(dir);
+  version = await bumpVersion(dir, config, getCommitVersion(config, commits), commits);
+
+  await run(dir, "git", "push", "origin", `refs/tags/v${version}`);
+  await run(dir, "git", "reset", "--soft", "HEAD^")
+  await run(dir, "git", "restore", "--staged", ".")
+  await run(dir, "git", "commit", "-a", "-m", `Release ${version}`).catch(e =>
+    e instanceof ExitError && e.code === 1 ? false : Promise.reject(e)
+  )
+  await run(dir, "git", "push")
+
+  console.log("Done.");
+}
+
+
+async function bumpVersion(dir, config, version, commits) {
+  const args = version ? ["--new-version", version] : [`--${getStrategyFromCommit(commits)}`]
+
+  await run(
+    dir,
+    "yarn",
+    "version",
+    ...args,
+  ).catch(e =>
+    e instanceof ExitError && e.code === 1 ? false : Promise.reject(e)
+  );
+
+  const newVersion = await getVersion(dir)
+  console.log(`New version: ${newVersion}`)
+  return newVersion;
+}
+
+
+function getCommitVersion(config, commits) {
+  for (const commit of commits) {
+    const match = commit.message.match(config.commitPattern);
+    if (match && match[1]) {
+      return match[1]
+    }
+  }
+  return null
+}
+
+function getStrategyFromCommit(commits) {
+
+  if (commits.some(({message}) =>
+    message.includes("BREAKING CHANGE") ||
+    message.toLowerCase().includes("major")
+  )) {
+    return "major"
+  }
+
+  if (commits.some(({message}) =>
+    message.toLowerCase().startsWith("feat") ||
+    message.toLowerCase().includes("minor")
+  )) {
+    return "minor"
+  }
+  return "patch"
+}
+
+
+async function readJson(file) {
+  const data = await new Promise((resolve, reject) =>
+    readFile(file, "utf8", (err, data) => {
+      if (err) {
+        reject(err);
+      } else {
+        resolve(data);
+      }
+    })
+  );
+  return JSON.parse(data);
+}
+
+async function gitSetup(dir, config) {
+  const {name, email} = config.tagAuthor;
+  await run(dir, "git", "config", "user.name", name);
+  await run(dir, "git", "config", "user.email", email);
+}
+
+async function addBuiltPackage(dir) {
+  await run(dir, "yarn");
+  await run(dir, "yarn", "build");
+  await run(dir, "git", "add", "-f", "dist")
+}
+
+function run(cwd, command, ...args) {
+  console.log("Executing:", command, args.join(" "));
+  return new Promise((resolve, reject) => {
+    const proc = spawn(command, args, {
+      cwd,
+      stdio: ["ignore", "ignore", "pipe"]
+    });
+    const buffers = [];
+    proc.stderr.on("data", data => buffers.push(data));
+    proc.on("error", () => {
+      reject(new Error(`command failed: ${command}`));
+    });
+    proc.on("exit", code => {
+      if (code === 0) {
+        resolve(true);
+      } else {
+        const stderr = Buffer.concat(buffers).toString("utf8").trim();
+        if (stderr) {
+          console.log(`command failed with code ${code}`);
+          console.log(stderr);
+        }
+        reject(new ExitError(code));
+      }
+    });
+  });
+}
+
+class ExitError extends Error {
+  constructor(code) {
+    super(`command failed with code ${code}`);
+    this.code = code;
+  }
+}
+
+class NeutralExitError extends Error {
+}
+
+if (require.main === module) {
+  main().catch(e => {
+    if (e instanceof NeutralExitError) {
+      // GitHub removed support for neutral exit code:
+      // https://twitter.com/ethomson/status/1163899559279497217
+      process.exitCode = 0;
+    } else {
+      process.exitCode = 1;
+      console.log(e.message || e);
+    }
+  });
+}
